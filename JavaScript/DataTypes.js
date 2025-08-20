42c1ed3553d790b0b6f20528f0d1a7329b429ed4
vinayg2509 <vinayg2509@gmail.com>
2025-08-20 14:57:06 +0530

    [Vinay]: add explanation about datatypes in JavaScript

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,35 @@
+// !---------------------------------DataType----------------------------------
+
+console.log("Data types and its example");
+
+// ^ 1.String
+
+    let name = "John";
+    let message = 'Hello';
+    console.log(message , name);
+
+// ^2.Bigint
+
+    let largeNumber = 9007199254740991n;
+    console.log(largeNumber);
+
+// ^3.number
+    let age = 25;
+    let price = 19.99;
+
+//^ 4.null
+  
+    let bike = null;
+    console.log(bike);
+    
+//^ 5.undefined
+  let car;
+  console.log(car); // Output: undefined
+
+
+// ^6.Symbol
+  const id = Symbol('id');
+   
+// ^7.Boolean
+   let isStudent = true;
+   let hasLicense = false;
